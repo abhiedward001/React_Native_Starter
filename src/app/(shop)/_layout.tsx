628bcf8e66ada8bb67185e4de0e4a73b888dbd4d
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-11-02 11:38:55 +0530

    feat: dont with the routes setup

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,16 +1,51 @@
+import React from 'react'
 import { Tabs } from "expo-router";
 import { View, Text } from 'react-native'
-import React from 'react'
+import { FontAwesome } from '@expo/vector-icons';
 import { SafeAreaView } from "react-native-safe-area-context";
 
+
+interface iconProviderProps{
+  color: string;
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+}
+const IconProvider = (props:iconProviderProps) => {
+  return <FontAwesome size={24} {...props} color='#1472ba'></FontAwesome>
+}
 const Tabslayout = () => {
   return (
-    <SafeAreaView>
-      <Tabs>
+    <SafeAreaView
+      edges={['top']}
+      style={{
+      flex: 1,
+    }}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#1472ba',
+          tabBarInactiveTintColor: 'gray',
+          tabBarLabelStyle: { fontSize: 16 },
+          tabBarStyle: {
+            borderTopLeftRadius: 16,
+            borderTopRightRadius: 16,
+            paddingTop:8
+          },
+          headerShown:false
+      }}
+      >
         <Tabs.Screen
           name='index'
           options={{
-            headerShown:true
+            tabBarIcon(props) {
+              return <IconProvider {...props} name='shopping-cart'></IconProvider>
+          }
+          }}
+        />
+         <Tabs.Screen
+          name='orders'
+          options={{
+            tabBarIcon(props) {
+              return <IconProvider {...props} name='book'></IconProvider>
+          }
           }}
         />
         
